d128f9b7bf0ca017161dc36271b495f854d76b86
Ferdinandio <dmi8829@yandex.ru>
2020-09-28 14:17:13 +0400

    mentor-paticipation переделал 28.09.2020

--- src/app/mentor-participation/mentor-participation.page.ts
+++ src/app/mentor-participation/mentor-participation.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mentor-participation',
   templateUrl: './mentor-participation.page.html',
   styleUrls: ['./mentor-participation.page.scss'],
 })
-export class MentorParticipationPage implements OnInit {
 
-  constructor() { }
+export class MentorParticipationPage {
 
-  ngOnInit() {
+  constructor(public toastController: ToastController) {}
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Данные успешно отправлены',
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
